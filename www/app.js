cc322bfc4f86efdc279cd77c26b99dee5d8365c0
Adri <85852773+AdriMataLuna@users.noreply.github.com>
2023-03-02 15:44:02 +0100

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,30 +1,45 @@
+/* establim l'url de conexió amb l'API */
 const BaseUrl = "http://localhost:3000/api/tasques";
 var llista = [];
 
+/* definim una constant data*/
 const fecha = new Date();
+
+/* degut a problemes amb el tractament de les dates entre codig html, MongoDB i NodeJS la millor opció
+ha estat crear un string amb la data actual perque tingui un format yyyy-mm-dd (si no es fa aixi
+quan el mes o dia es un sol digit no agafa el valor "0X" sino "X", i dona errors) */
+
 if((fecha.getMonth()+1) < 10 && fecha.getDate() < 10){
     var dataact = (fecha.getFullYear()+`-0`+(fecha.getMonth()+1)+`-0`+fecha.getDate());
 
 }else if((fecha.getMonth()+1) < 10 && fecha.getDate() >= 10){
     var dataact = (fecha.getFullYear()+`-0`+(fecha.getMonth()+1)+`-`+fecha.getDate());
 
-}else{
+}else if((fecha.getMonth()+1) >= 10 && fecha.getDate() < 10){
+    var dataact = (fecha.getFullYear()+`-`+(fecha.getMonth()+1)+`-0`+fecha.getDate());
+
+}else {
     var dataact = (fecha.getFullYear()+`-`+(fecha.getMonth()+1)+`-`+fecha.getDate());
 }
 
 
+/* creem una funció per printar les dades quan iniciem la pàgina */
 function inicialitzacio(){
+
+    /* seleccionem les seccions on printarem les dades */
     var zonaTodo = document.getElementById("todo");
     var zonaDoing = document.getElementById("doing");
     var zonaDone = document.getElementById("done");
 
+    /* fem una crida a l'api perque ens retorni les dades de la bdd mongo */
+
      fetch(BaseUrl).then((res) => {
         if(res.ok) {
             res.json().then((dades) => {
-                
- 
+                /* un cop tenim les dades i les hem pasat a un formatat, crearem un bucle per recorrer les dades */
                 for(i = 0;i < dades.length;i++){
-
+                    
+                    /* primer creem l'estructura que printarem amb les dades pertinents */
                     var cosNota = `
                       <div class="tasca row align-items-start justify-content-lg-center" style="height: 275px;">
                         <div id="primari" class="col-10 col-sm-10 col-md-10 col-lg-9 bg-transparent border `+dades[i].prioritat+` border-4" style="height: 100%">
@@ -65,6 +80,7 @@ function inicialitzacio(){
                     </div>
                       </div>`
 
+                      /* un cop ja tenim l'estructura nota mirem en quina posició està i la printem */
                     if(dades[i].position == 'todo'){
                       zonaTodo.innerHTML += cosNota
                     }else if(dades[i].position == 'doing'){
@@ -73,6 +89,8 @@ function inicialitzacio(){
                       zonaDone.innerHTML += cosNota
                     }
                   }
+
+                  /* afegim funcions als botons */
                   addfunctions();
             } )
         } else {
@@ -84,10 +102,13 @@ function inicialitzacio(){
     })
 } 
 
+/* creem una funció per afegir elements nous */
 function newElement(){
 
+    /* seleccionem la zona on printarem les dades */
     var zonaTodo = document.getElementById("todo");
 
+    /* creem una estructura tasca bàsica */
         var aux = {
 
             "titol":"Titol de la tasca",
@@ -99,6 +120,9 @@ function newElement(){
             "position":"todo"
         };
        
+        /* fem una crida a l'api amb metode POST, per enviar l'estructura que acabem de crear,
+        i que ens pugi retornar la ID que assigna automaticament el mongo, ja que hem programat l'api
+        per retornar resposta també */
         fetch(BaseUrl, 
         {
             method: "POST",
@@ -109,9 +133,13 @@ function newElement(){
         })
             .then((res) => {
                 if(res.ok) {
+
                     res.json().then((res2) => {
+                        /* un cop pujada l'estructura bàsica consultem quina ID se l'ha assignat a la nota
+                        i podrem printar-la amb la seva respectiva id assignada al MongoDB */
                         const id = res2[0]._id;
 
+                        /* creem una estructura bàsica de la nota amb l'estructura que acabem de rebre mitjançant l'api desde el mongo */
                 var cosNota = ` 
                 <div class="tasca row align-items-start justify-content-lg-center" style="height: 275px;">
                 <div id="primari" class="col-10 col-sm-10 col-md-10 col-lg-9 bg-info border border-info border-4" style="height: 100%">
@@ -151,9 +179,9 @@ function newElement(){
                     </div>
                   </div>
                 </div>`
-            
+
+                /* printem la nota a la secció principal i afegim funcions als botons */
                 zonaTodo.innerHTML += cosNota;
-                
                 addfunctions();
                     })
                 } else {
@@ -164,6 +192,7 @@ function newElement(){
                 })    
 }
 
+/* creem una funció per afegir funcions als botons */
 function addfunctions(){
     //definim els botons per aplicar cada funció, del(eliminar), add(afegir al LocalStorage), move(modificar l'estat), prio(modificar prioritat), canvi(marcar a l'usuari que hi han canvis sense desar)
     var del = document.getElementsByClassName("del");
@@ -176,14 +205,13 @@ function addfunctions(){
     for (i = 0; i < canvi.length; i++){
 
         canvi[i].onclick = function() {
-        //seleccionem els elements que necesitem
+        /*seleccionem els elements que necesitem*/
         var containerNota = this.parentElement;
         var containerButtons = containerNota.nextElementSibling;
-        //mirem quina prioritat té la nota
+        /*mirem quina prioritat té la nota */
         var prio = containerNota.querySelector('#prioritat').innerHTML;
         this.contentEditable = true;
-
-        //modifiquem el color per identificarla com a nota no desada
+        /*modifiquem el color per identificarla com a nota "no desada" i modifiquem el tamany dels botons*/
         containerNota.classList.remove(prio);
         containerNota.classList.add('border-info');
         containerNota.classList.remove('bg-transparent');
@@ -192,46 +220,47 @@ function addfunctions(){
         containerButtons.classList.add('border-info');
 
         containerButtons.innerHTML = `
-<div class="save"> 
-<svg xmlns="http://www.w3.org/2000/svg"  width="31px" height="31px" fill="currentColor" style="color:#00BE4F;" class="bi bi-save-fill mt-2" viewBox="0 0 16 16">
-    <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
-</svg>
-</div>
-<div class="del"> 
-<svg xmlns="http://www.w3.org/2000/svg" width="31px" height="31px" fill="currentColor" style="color:#E91E63;" class="bi bi-trash-fill mt-2" viewBox="0 0 16 16">
-    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-</svg>
-</div>
-<div class="move"> 
-<svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="currentColor" style="color:#FFAE00;" class="bi bi-arrow-right-square-fill mt-2" viewBox="0 0 16 16">
-    <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
-</svg>
-</div>
-<div class="prio"> 
-<svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="currentColor" style="color: #2c2fa5;" class="bi bi-palette-fill mt-2" viewBox="0 0 16 16">
-    <path d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-</svg>
-</div>
-</div>
-</div>`;
+            <div class="save"> 
+                <svg xmlns="http://www.w3.org/2000/svg"  width="31px" height="31px" fill="currentColor" style="color:#00BE4F;" class="bi bi-save-fill mt-2" viewBox="0 0 16 16">
+                    <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+                </svg>
+            </div>
+            <div class="del"> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="31px" height="31px" fill="currentColor" style="color:#E91E63;" class="bi bi-trash-fill mt-2" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                </svg>
+            </div>
+            <div class="move"> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="currentColor" style="color:#FFAE00;" class="bi bi-arrow-right-square-fill mt-2" viewBox="0 0 16 16">
+                    <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z"/>
+                </svg>
+            </div>
+            <div class="prio"> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="currentColor" style="color: #2c2fa5;" class="bi bi-palette-fill mt-2" viewBox="0 0 16 16">
+                    <path d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                </svg>
+            </div>
+        </div>
+    </div>`;
 
+    /* afegim funcions ja que al re-printar la secció botons s'han perdut la conexió amb les funcions */
     addfunctions();
 
     }
 
     } 
-        //fem un recorregut per aplicar funcions pròpies de botonera a cada nota
-    for(i = 0; i < add.length; i++){
 
-    //creem la funció add(afegir al LocalStorage)
+/*fem un recorregut per aplicar funcions pròpies de botonera a cada nota*/
+for(i = 0; i < add.length; i++){
+    /*creem la funció add(afegir al LocalStorage)*/
     add[i].onclick = function() {
-        //escalem per l'element pare fins arribar a seleccionar la "nota" completament
+        /* escalem per l'element pare fins arribar a seleccionar la "nota" completament */
         var containerButtons = this.parentElement;
         var nota = containerButtons.parentElement;
         var containerNota = nota.firstElementChild;
-        //primer mirarem quina id té
+        /* primer mirarem quina id té */
         var iden = nota.querySelector('#num').innerHTML;
-        //ara seleccionem el contingut del div
+        /* ara seleccionem el contingut del div */
         var tit = nota.querySelector('#titol').innerHTML;
         var cont = nota.querySelector('#contingut').innerHTML;
         var pos = nota.querySelector('#posi').innerHTML;
@@ -240,7 +269,7 @@ function addfunctions(){
         var iniciD = nota.querySelector('#start').value;
         var finalD = nota.querySelector('#finish').value;
         
-        //creem una estructura per guardar les dades seleccionades
+        /* creem una estructura per guardar les dades seleccionades */
         let aux = {
         "prioritat": prio,
         "titol": tit,
@@ -251,7 +280,7 @@ function addfunctions(){
         "dataFin": finalD
         }
         
-
+        /* fem una crida a l'api i actualitzem les dades */
         fetch(BaseUrl + "/" + iden, 
 {
     method: "PUT",
@@ -272,14 +301,14 @@ function addfunctions(){
     })
 
 
-        //treiem l'estat "modificat" i tornem a deixar l'estat que tenia anteriorment
+        /* treiem l'aspecte "modificat" i tornem a deixar la prioritat que tenia anteriorment */
         containerNota.classList.remove('border-info');
         containerNota.classList.add(prio);
         containerNota.classList.remove('bg-info');
         containerNota.classList.add('bg-transparent');
         containerButtons.classList.remove('border-info');
         containerButtons.classList.add(prio);
-        //tornem a afegir la botonera amb la mesura normal
+        /* tornem a afegir la botonera amb la mesura normal */
         containerButtons.innerHTML = `
         <div class="save"> 
         <svg xmlns="http://www.w3.org/2000/svg"  width="31px" height="31px" fill="currentColor" style="color:#00BE4F;" class="bi bi-save-fill mt-2" viewBox="0 0 16 16">
@@ -304,18 +333,21 @@ function addfunctions(){
     </div>
         </div>`;
 
+        /* fem una crida per afegir funcions a la nova botonera */
         addfunctions();
 
     }
 
+    /* creem una funció per eliminar tasques */
     del[i].onclick = function() {
-        //seleccionem els elements necesaris i busquem la id de la nota
+        /* seleccionem els elements necesaris i busquem la id de la nota */
         var containerButtons = this.parentElement;
         var nota = containerButtons.parentElement;
         var iden = nota.querySelector('#num').innerHTML;
-        //apliquem style none perque desaparegui de la vista de l'usuari
+        /* apliquem style none perque desaparegui de la vista de l'usuari */
         nota.style.display = "none";
 
+        /* fem una crida a l'API per eliminar la tasca mitjançant l'ID */
         fetch(BaseUrl + "/" + iden, 
 {
     method: "DELETE",
@@ -332,11 +364,13 @@ function addfunctions(){
         console.log("Error: ", err);
     })
 
+    /* fem una crida per afegir funcions als botons */
     addfunctions();
     }
 
+    /* fem una funció per poder moure la tasca de posició */
     move[i].onclick = function (){
-        //primer adquirim les dades que necesitem
+        /* primer adquirim les dades que necesitem */
         var containerButtons = this.parentElement;
         var nota = containerButtons.parentElement;
         var iden = nota.querySelector('#num').innerHTML;
@@ -348,7 +382,7 @@ function addfunctions(){
         var iniciD = nota.querySelector('#start').value;
         var finalD = nota.querySelector('#finish').value;
 
-        //mirem quina posició té actualment i guardem la seguent en una variable auxiliar.
+        /* mirem quina posició té actualment i guardem la seguent en una variable auxiliar. */
         if (loc == 'todo'){
             locaux = 'doing';
         }else if(loc == 'doing'){
@@ -356,7 +390,7 @@ function addfunctions(){
         }else{
             locaux = 'todo';
         }
-        //creem l'estructura d'informació i tornem a guardar-ho al local storage.
+        /* creem l'estructura d'informació i tornem a guardar-la amb la nova posició. */
         let aux = {
             "prioritat": prio,
             "titol": tit,
@@ -367,6 +401,7 @@ function addfunctions(){
             "dataFin": finalD
             }
     
+            /* fem una crida a l'API per actualitzar les dades */
             fetch(BaseUrl + "/" + iden, 
             {
                 method: "PUT",
@@ -378,6 +413,7 @@ function addfunctions(){
                 if(res.ok) {
                     res.json()
                         .then((res2) => {
+                            /* si tot va bé recarregem la pàgina perque es mostrin les notes */
                             window.location.reload();
                         })
                 } else {
@@ -387,20 +423,22 @@ function addfunctions(){
             .catch((err) => {
                 console.log("Error: ", err);
             })
+
+            /* afegim funcions a la botonera */
             addfunctions();
     }
 
+    /* creem una funció per modificar la prioritat de la tasca */
     prio[i].onclick = function () {
-        /* 
+        /* treballarem segons aquest criteri:
         COLOR VERD = border-success
         COLOR GROC = border-warning
         COLOR VERMELL = border-danger*/
     
-        //primer adquirim les dades que necesitem
+        /* primer adquirim les dades que necesitem */
         var containerButtons = this.parentElement;
         var nota = containerButtons.parentElement;
         var containerNota = nota.firstElementChild;
-    
         var iden = nota.querySelector('#num').innerHTML;
         var tit = nota.querySelector('#titol').innerHTML;
         var cont = nota.querySelector('#contingut').innerHTML;
@@ -410,8 +448,7 @@ function addfunctions(){
         var iniciD = nota.querySelector('#start').value;
         var finalD = nota.querySelector('#finish').value;
 
-    
-            //mirem quina posició té actualment i guardem la seguent en una variable auxiliar.
+        /* mirem quina posició té actualment i guardem la seguent en una variable auxiliar */
         if (prio == 'border-success'){
             prio = 'border-warning';
             containerNota.classList.remove('border-success');
@@ -434,7 +471,7 @@ function addfunctions(){
             containerButtons.classList.add('border-success');
         }
     
-                    //creem l'estructura d'informació i tornem a guardar-ho al local storage.
+                /* creem l'estructura d'informació i tornem a guardar-la amb la nova prioritat. */
                     let aux = {
                     "prioritat": prio,
                     "titol": tit,
@@ -444,7 +481,8 @@ function addfunctions(){
                     "dataIni": iniciD,
                     "dataFin": finalD
                     }
-            
+
+                    /* fem una crida a l'API per actualitzar les dades */
                     fetch(BaseUrl + "/" + iden, 
                     {
                         method: "PUT",
